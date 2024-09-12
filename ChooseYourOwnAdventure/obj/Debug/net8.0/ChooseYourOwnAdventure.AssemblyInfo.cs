7bf487ff31dd0c3e7dee0f1b1e30ece44b201ae3
Anshu Anand <anshu.anand@myob.com>
2024-09-13 09:10:52 +1000

    Initialize the game

--- ChooseYourOwnAdventure/obj/Debug/net8.0/ChooseYourOwnAdventure.AssemblyInfo.cs
+++ ChooseYourOwnAdventure/obj/Debug/net8.0/ChooseYourOwnAdventure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChooseYourOwnAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f96d2ef0930cbbc123dd517a8cc1ec071093148")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d74781d173e6a6b65acf37a55af11f4629c39a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChooseYourOwnAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChooseYourOwnAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PasswordChecker/obj/Debug/net8.0/PasswordChecker.AssemblyInfo.cs
+++ PasswordChecker/obj/Debug/net8.0/PasswordChecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordChecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e53be6dde56dde8ef1a4e1891c3c1014c251f13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d74781d173e6a6b65acf37a55af11f4629c39a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordChecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
